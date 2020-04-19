517109e973872f8674efab233a26fc15b9d9f338
AlkVo <halfsteamedbun@gmail.com>
2020-04-19 16:05:47 +0800

    修改title

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
 
-    w.setWindowTitle(QStringLiteral("获取文件夹内制定类型文件的名称"));
+    w.setWindowTitle(QStringLiteral("获取文件夹内所有被指定类型文件的名称"));
     w.setWindowIcon(QIcon(":/icon/images/icon.png"));
     w.show();
 
